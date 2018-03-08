d31bece1c0e167a6d42fa014989675bb5c308610
Nao Minami <south37777@gmail.com>
2018-03-09 00:53:01 +0900

    Use rolling hash

--- string_search/string_search.cpp
+++ string_search/string_search.cpp
@@ -5,51 +5,45 @@
 
 using namespace std;
 
+typedef unsigned long long ull;
+const ull B = 100000007;
+
 string T;
 string P;
-
-#define MAX_DIS 256
-int dis[MAX_DIS];
-
-int bm(int i) {
-  for (int j = 0; j < P.size(); ++j) {
-    // NOTE: for debug
-    // cout << "compare " << T[i - j] << " and " << P[P.size() - 1 - j] << endl;
-
-    if (T[i - j] != P[P.size() - 1 - j]) {
-      // NOTE: update i
-      return max(dis[T[i - j]] - j, 1);
-    }
-  }
-  return 0;  // means `find`
-}
 
 int main(int argc, char** argv) {
   cin >> T;
   cin >> P;
 
-  for (int i = 0; i < MAX_DIS; ++i) {
-    dis[i] = P.size();
-  }
-  for (int i = 0; i < P.size(); ++i) {
-    dis[P[i]] = P.size() - 1 - i;
-  }
-  // NOTE: for debug
-  // cout << "dis['a']=" << dis['a'] << endl;
-  // cout << "dis['b']=" << dis['b'] << endl;
+  int ts = T.size();
+  int ps = P.size();
+  // NOTE: Early return
+  if (ps > ts) return 0;
 
+  // NOTE: calculate t = B ^ ps
+  ull t = 1;
+  for (int i = 0; i < ps; ++i) {
+    t *= B;
+  }
+
+  ull th = 0;
+  ull ph = 0;
+  // NOTE: calculate for fist ps
+  for (int i = 0; i < ps; ++i) {
+    th = th * B + T[i];
+  }
+  for (int i = 0; i < ps; ++i) {
+    ph = ph * B + P[i];
+  }
+
+  // NOTE: calculate result
   vector<int> r;
-  int i = P.size() - 1;
-  while (i < T.size()) {
-    // NOTE: for debug
-    // cout << "try " << i << endl;
-
-    int d = bm(i);
-    if (d == 0) { // find
-      r.push_back(i - (P.size() - 1));
-      ++i;
-    } else {
-      i += d;
+  for (int i = 0; i + ps <= ts; ++i) {
+    if (ph == th) {  // NOTE: match `b[i] .. b[i + ps - 1]`
+      r.push_back(i);
+    }
+    if (i + ps < ts) {
+      th = th * B + T[i + ps] - T[i] * t;
     }
   }
 
