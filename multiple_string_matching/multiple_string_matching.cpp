b430e625230044154e6498a87a87b11ec7cbf66a
Nao Minami <south37777@gmail.com>
2018-03-11 15:26:09 +0900

    Update

--- multiple_string_matching/multiple_string_matching.cpp
+++ multiple_string_matching/multiple_string_matching.cpp
@@ -1,112 +1,79 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <cstring>
 
-using namespace std;
+#define MAX_LEN 1000005
 
-int Rank[1000005];
-int Tmp[1000005];
-int sa[1000005];
-int kc, len;
+int sa[MAX_LEN];
+int Rank[MAX_LEN];
+int Tmp[MAX_LEN];
+int k, len;
 
 bool compare_sa(int i, int j) {
   return Rank[i] != Rank[j] ?
     Rank[i] < Rank[j] :
-    (i + kc <= len ? Rank[i + kc] : -1) < (j + kc <= len ? Rank[j + kc] : -1);
+    (i + k <= len ? Rank[i + k] : -1) < (j + k <= len ? Rank[j + k] : -1);
 }
 
-void construct_sa(string& T) {
+void construct_sa(std::string& T) {
   len = T.size();
   for (int i = 0; i <= len; ++i) {
     sa[i] = i;
     Rank[i] = (i < len) ? T[i] : -1;
   }
-
-  // NOTE: only for debug
-  // cout << "sa: " << endl;
-  // for (int i = 0; i < len + 1; ++i) {
-  //   cout << sa[i] << ": " << T.c_str() + sa[i] << endl;
-  // }
-
-  for (kc = 1; kc <= len; kc *= 2) {
-    sort(sa, sa + len + 1, compare_sa);
-
-    // NOTE: only for debug
-    // cout << "sa: " << endl;
-    // for (int i = 0; i < len + 1; ++i) {
-    //   cout << sa[i] << ": " << T.c_str() + sa[i] << endl;
-    // }
-
+  for (k = 1; k <= len; k *= 2) {
+    std::sort(sa, sa + len + 1, compare_sa);
     Tmp[sa[0]] = 0;
     for (int i = 1; i <= len; ++i) {
-      Tmp[sa[i]] = Tmp[sa[i-1]] + compare_sa(sa[i-1], sa[i]);
+      Tmp[sa[i]] = Tmp[sa[i - 1]] + compare_sa(sa[i - 1], sa[i]);
     }
     for (int i = 0; i <= len; ++i) {
       Rank[i] = Tmp[i];
     }
-
-    // cout << "kc: " << kc << endl;
-    // cout << "Rank: " << endl;
-    // for (int i = 0; i < len + 1; ++i) {
-    //   cout << "Rank[" << i << "]" << ": " << Rank[i] << endl;
-    // }
   }
-
-  // NOTE: only for debug
-  // cout << "sa: " << endl;
-  // for (int i = 0; i < len; ++i) {
-  //   cout << sa[i] << endl;
-  // }
 }
 
 int main(int argc, char** argv) {
-  string T;
-  cin >> T;
+  std::string T;
+  std::cin >> T;
   int n = T.size();
   construct_sa(T);
+
   // NOTE: only for debug
-  // cout << "sa: " << endl;
-  // for (int i = 0; i < n; ++i) {
-  //   cout << sa[i] << ": " << T.c_str() + sa[i] << endl;
+  // for (int i = 0; i <= n; ++i) {
+  //   std::cout << "sa[" << i << "]: " << sa[i] << ", T.c_str() + sa[i]: " << T.c_str() + sa[i] << std::endl;
   // }
 
   int Q;
-  cin >> Q;
+  std::cin >> Q;
   for (int i = 0; i < Q; ++i) {
-    string P;
-    cin >> P;
+    std::string P;
+    std::cin >> P;
 
     // NOTE: do this
-    // cout << P << endl;
-    int k = P.size();
+    bool success = false;
     int l = 1;
     int r = n;
-    int m;
-    int success = false;
     while (l <= r) {
-      m = (l + r) / 2;
-      int cmp = strncmp(T.c_str() + sa[m], P.c_str(), k);
-      // NOTE: only for debug
-      // cout << "m: " << m << ", sa[m]: " << sa[m] << ", T.c_str() + sa[m]: " << T.c_str() + sa[m] << endl;
-      // cout << "cmp: " << cmp << endl;
-
+      int m = (l+r)/2;
+      int cmp = std::strncmp(T.c_str() + sa[m], P.c_str(), P.size());
       if (cmp == 0) {
-        // NOTE: success
         success = true;
         break;
-      } else if (cmp > 0) { // P is small
+      } else if (cmp > 0) { // positive means P is small than T[sa[m]..sa[m]+P.size]
         r = m - 1;
-      } else {  // cmp < 0, P is large
+      } else {  // cmp < 0
         l = m + 1;
       }
     }
 
     if (success) {
-      cout << 1 << endl;
+      std::cout << 1 << std::endl;
     } else {
-      cout << 0 << endl;
+      std::cout << 0 << std::endl;
     }
   }
+
   return 0;
 }
